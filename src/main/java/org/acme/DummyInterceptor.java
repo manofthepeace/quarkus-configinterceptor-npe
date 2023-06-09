79bf9984d71ff82a786735cd24f733d73c94a5d2
Alex Martel <13215031+manofthepeace@users.noreply.github.com>
2023-06-09 13:15:17 -0400

    Add null check for config interceptor

--- src/main/java/org/acme/DummyInterceptor.java
+++ src/main/java/org/acme/DummyInterceptor.java
@@ -1,5 +1,7 @@
 package org.acme;
 
+import org.jboss.logging.Logger;
+
 import io.smallrye.config.ConfigSourceInterceptor;
 import io.smallrye.config.ConfigSourceInterceptorContext;
 import io.smallrye.config.ConfigValue;
@@ -8,11 +10,18 @@ public class DummyInterceptor implements ConfigSourceInterceptor {
 
     private static final String THREAD_KEEP_ALIVE = "quarkus.thread-pool.keep-alive-time";
 
+    private static final Logger LOG = Logger.getLogger(DummyInterceptor.class);
+
     @Override
     public ConfigValue getValue(ConfigSourceInterceptorContext context, String name) {
         String keepAlive = "60s";
         if (name.equals(THREAD_KEEP_ALIVE)) {
-            return context.proceed(name).withValue(keepAlive);
+            ConfigValue configValue = context.proceed(name);
+            if (configValue != null) {
+                LOG.infof("keepalive config set to %s", keepAlive);
+                configValue = configValue.withValue(keepAlive);
+            }
+            return configValue;
         }
         return context.proceed(name);
     }
